14bfd00a33c377171550b8eef543c515b9785e87
set <set>
2023-09-30 19:13:56 -0300

    Adds basic test to add text to screen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("something")
+window.onload = () => {
+    const text = document.createElement("span");
+    const content = document.createTextNode("text");
+    text.appendChild(content)
+    document.getElementById("main").appendChild(text);
+};
