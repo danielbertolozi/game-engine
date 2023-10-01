c2cb75a2a4692533f9f13e74f30c4b06f16f4a94
set <set>
2023-10-01 00:22:47 -0300

    Implement loop

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
+function getCounter() {
+  let i = 0;
+  return () => {
+    return i++;
+  }
+}
+
+const counter = getCounter();
+let text;
+function draw() {
+  return new Promise((resolve, reject) => {
+    if (!text) {
+      text = document.createElement("span");
+      const content = document.createTextNode(counter());
+      text.appendChild(content)
+      document.getElementById("main").appendChild(text);
+    } else {
+      text.childNodes[0].textContent = counter();
+    }
+    resolve();
+  });
+}
+
+function drawLoop() {
+  return draw().then(() => {
+    setTimeout(drawLoop, timeBetweenFramesInMillis);;
+  })
+}
+
+const timeBetweenFrames = 0.0166666667;
+const timeBetweenFramesInMillis = 16.6666667;
 window.onload = () => {
-    const text = document.createElement("span");
-    const content = document.createTextNode("text");
-    text.appendChild(content)
-    document.getElementById("main").appendChild(text);
+  drawLoop();
 };
